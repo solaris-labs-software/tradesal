341b9af54f4b89c51e53f7c6cafad993a0d88ff1
SantiGrimoldi <Santiago.grimoldi@ing.austral.edu.ar>
2024-08-12 11:33:22 -0300

    feat: theme presentatio

---
+++ src/components/common/presentation.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import '/src/styles/variables.scss';
+
+const Presentation = ({ title, text }) => {
+    return (
+        <div className="relative bg-cover bg-center w-full h-52 flex justify-center items-center">
+            <div
+                className="absolute inset-0 bg-black opacity-95"
+                style={{
+                    backgroundImage: 'url(/src/assets/bg-presentation.jpg)',
+                    backgroundSize: 'cover',
+                    backgroundPosition: 'center',
+                    filter: 'brightness(70%)', // Darken the background
+                }}
+            ></div>
+            <div className="relative z-1 flex flex-col items-center">
+                <h1 className='text-white text-6xl font-bold mb-5'>{title}</h1>
+                <p className='text-white text-xl'>{text}</p>
+            </div>
+        </div>
+    );
+};
+
+export default Presentation;
--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,19 +1,23 @@
 import React from 'react';
 import '/src/styles/variables.scss';
+import Presentation from '../common/presentation';
 
 const Navbar = () => {
     const values = ['Home', 'About', 'Services', 'Contact'];
     return (
-        <nav className='flex justify-between bg-primary py-5 align-middle'>
-            <h1 className='text-white ml-5 text-2xl'>Logo</h1>
-            <ul className='flex justify-end gap-10'>
-                {
-                    values.map((value, index) => {
-                        return <button key={index} className='text-white mx-10'>{value}</button>
-                    })
-                }
-            </ul>
-        </nav>
+        <>
+            <nav className='flex justify-between bg-primary py-5 align-middle'>
+                <h1 className='text-white ml-5 text-2xl'>Logo</h1>
+                <ul className='flex justify-end gap-10'>
+                    {
+                        values.map((value, index) => {
+                            return <button key={index} className='text-white mx-10 text-xl'>{value}</button>
+                        })
+                    }
+                </ul>
+            </nav>
+            <Presentation title='CURSOS Y CAPACITACIONES' text='"Impulsa tu futuro con cada aprendizaje"' />
+        </>
     );
 };
 
