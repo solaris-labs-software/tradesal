2882a37ca00ad2550e27d2e6569b14fa87f34731
quinhos <marco.castagnaro@ing.austral.edu.ar>
2024-08-16 15:02:37 -0300

    missing some info and correct fotos

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -9,13 +9,13 @@ export const Footer = () => {
                         <h3 className="text-2xl font-bold mb-4">Contact Us</h3>
                         <div className="flex flex-col space-y-4">
                             <div className="flex items-center space-x-4">
-                                <img src={phone} alt="phone" className="w-6 h-6"/>
-                                <span className="text-white">Phone: +54 9 11 3256-8564</span>
+                                <img src={phone} alt={"phone"} className="w-6 h-6"/>
+                                <span className="text-white">Phone: +54 9-11 6265 1619</span>
                             </div>
-                            <div className="flex items-center space-x-4">
-                                <img src={email} alt="email" className="w-6 h-6"/>
-                                <span className="text-white">Email: info@yourcompany.com</span>
-                            </div>
+                            {/*<div className="flex items-center space-x-4">*/}
+                            {/*    <img src={email} alt={"email"} className="w-6 h-6"/>*/}
+                            {/*    <span className="text-white">Email: info@yourcompany.com</span>*/}
+                            {/*</div>*/}
                         </div>
                     </div>
                     <div className="md:w-1/3 flex justify-end">
@@ -27,6 +27,5 @@ export const Footer = () => {
                 </div>
             </div>
         </footer>
-
     );
 };
